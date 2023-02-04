ebedf3dd153786d57a70ace57f2dbb87aa238136
Evan <evanz1215@gmail.com>
2023-02-04 18:15:50 +0800

    新增 Vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        plugins: [
+            vue(),
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+            },
+        }
+    });
+}
+
